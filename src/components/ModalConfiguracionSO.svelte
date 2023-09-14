30ed7d8fed6a7b4261acac9301862f38dfdb6567
JEMM6192 <82985979+JEMM6192@users.noreply.github.com>
2023-09-14 11:26:10 -0600

    correciones en modal configuracion SO.

--- src/components/ModalConfiguracionSO.svelte
+++ src/components/ModalConfiguracionSO.svelte
@@ -5,34 +5,16 @@
 
 <Modal title="Configuracion SO" bind:open={showModalConfiguracionSO} autoclose>
   <div class="mb-6">
-    <Label for="default-input" class="block mb-2">Memoria Virtual</Label>
+    <Label for="default-input" class="block mb-2"
+      >Cantidad de Marco de paginas a utilizar</Label
+    >
     <Input
       type="number"
       id="default-input"
       placeholder="Cantidad Memoria Virtual (GB)"
     />
   </div>
-  <div class="mb-6">
-    <Label for="default-input" class="block mb-2">Memoria Fisica</Label>
-    <Input
-      type="number"
-      id="default-input"
-      placeholder="Cantidad Memoria Fisica (GB)"
-    />
-  </div>
-  <div class="mb-6">
-    <Label for="default-input" class="block mb-2">Tabla de paginas</Label>
-    <Input type="number" id="default-input" placeholder="Tabla de paginas" />
-  </div>
-  <div class="mb-6">
-    <Label for="default-input" class="block mb-2">Algoritmo de paginacion</Label
-    >
-    <select name="select">
-      <option value="value1">Optimo</option>
-      <option value="value2">No usandas reciente,mente</option>
-      <option value="value3">Fifo</option>
-    </select>
-  </div>
+
   <svelte:fragment slot="footer">
     <Button on:click={() => alert('Handle "success"')}>Guardar</Button>
     <Button color="alternative">Cancelar</Button>
